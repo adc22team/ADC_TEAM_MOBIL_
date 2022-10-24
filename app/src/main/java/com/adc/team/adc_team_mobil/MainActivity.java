853d7a8d1426a207999bfca80d083fdbbc94df08
adc22team <adc22team@gmail.com>
2022-10-25 00:36:21 +0200

    second Commit

---
+++ app/src/main/java/com/adc/team/adc_team_mobil/MainActivity.java
@@ -0,0 +1,166 @@
+package com.adc.team.adc_team_mobil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * Clase que representa la pantalla del Login de usuario movil
+ * @author Daniela Gutierrez
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    //Declaración de variables
+    Button btnIniciarSesion;
+    EditText etUsuario, etClave;
+    ProgressBar progressBar;
+    int resposta_id;
+    int resposta_rol;
+    private static final String TAG = "Resposta server :";
+
+    /**
+     *Inicia la actividad
+     * @param savedInstanceState guarda el estado de la pantalla
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //asignamos en cada variable el recurso correspondiente definido en el XML, para poder acceder a cada  uno de ellos
+
+        btnIniciarSesion = (Button) findViewById(R.id.btn_login);
+        etUsuario = (EditText) findViewById(R.id.editTextUsuari);
+        etClave = (EditText) findViewById(R.id.editTextPwd);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+
+        /**
+         * Acción del botón Login para recoger el nombre del usuario
+         */
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Task1().execute(etUsuario.getText().toString());
+            }
+        });
+    }
+
+    /**
+     * Desarrollo de la acción recoger el nombre del usuario
+     * Extiende la clase AsyncTask que permite ejecutar tareas en segundo plano, a manera de hilos
+     */
+    class Task1 extends AsyncTask<String,Void, String> {
+        /**
+         * Métdodo Override onPreExecute()
+         * Antes de comenzar el hilo, el botón de Login no es visible, osea no está activo
+         */
+        @Override
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+            btnIniciarSesion.setEnabled(false);
+
+        }
+        public boolean validarCampos (String etUsuario, String etClave){
+            if(etUsuario.isEmpty() || etClave.isEmpty()){
+                //Toast.makeText(this,"Por favor ingrese usuario y contraseña ", Toast.LENGTH_LONG).show();
+
+            }
+            return false;
+        }
+
+        /**
+         * Método Override doInBackGround
+         * Al comenzar el hilo, ejecutamos la acción en segundo plano, iniciando la conexión
+         * @param strings
+         * @return
+         */
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                //Intentamos establecer conexión con el servidor
+                Socket sc;
+                sc = new Socket("192.168.0.15", 5000);
+                DataInputStream in = new DataInputStream(sc.getInputStream());
+                DataOutputStream out = new DataOutputStream(sc.getOutputStream());
+
+                //Variable que recibirá la respuesta del servidor una vez establecida la conexión
+                String resposta_svr = in.readUTF();
+
+                //Métodos Log que muestran información en el logcat
+
+                // Se recibe la respuesta del server SERVER_SHOW_ELHO_established connection y se la muestra en logcat
+                Log.i(TAG, resposta_svr);
+                //Muestra la cadena del Usuario
+                Log.i(TAG, String.valueOf(String.valueOf(etUsuario.getText().toString())));
+                //Muestra la cadena del password
+                Log.i(TAG, String.valueOf(String.valueOf(etClave.getText().toString())));
+
+                //Enviamos respuesta al servidor con el usuario, contraseña y valor 0
+                out.writeUTF("LOGIN," + String.valueOf(etUsuario.getText().toString()) + "," + String.valueOf(etClave.getText().toString()) + "," + "0");
+
+                //Variable a la que se le asigna el valor entero del id de conexión del usuario y que recibimos del Servidor al establecer conexión
+                resposta_id = in.readInt();
+                //Método Log que muestra el valor de la respuesta
+                Log.i(TAG, String.valueOf(resposta_id));
+
+                if(resposta_id !=0){
+                    resposta_rol = in.readInt();
+                    Log.i(TAG, String.valueOf(resposta_rol));
+                    if(resposta_rol!=3){
+                        out.writeUTF("USER_EXIT");
+                    }else{
+                        Log.i(TAG,"Bienvenido");
+                    }
+                }
+            } catch (IOException  e) {
+                e.printStackTrace();
+            }
+
+            return strings[0];
+        }
+
+        /**
+         * Método que ejecuta la acción de la pantalla principal
+         * @param s
+         */
+        @Override
+        protected void onPostExecute(String s) {
+
+            progressBar.setVisibility(View.INVISIBLE);
+            btnIniciarSesion.setEnabled(true);
+
+            Log.i(TAG, "Valor de resposta_id:"+String.valueOf(resposta_id));
+            //Si el usuario tiene valor de Id de conexión, accede a la pantalla de Inicio
+            if (resposta_id != 0) {
+                Log.i(TAG, String.valueOf(resposta_rol));
+
+                Intent intent = new Intent(MainActivity.this, PantallaPrincipal.class);
+                Log.i(TAG, String.valueOf(resposta_id));
+                Log.i(TAG, String.valueOf(resposta_rol));
+                intent.putExtra("usuari", etUsuario.getText().toString());
+                intent.putExtra("pwd", etClave.getText().toString());
+                intent.putExtra("id", String.valueOf(resposta_id));
+                intent.putExtra("rol", String.valueOf(resposta_rol));
+                startActivity(intent);
+        }
+    }
+  }
+}
---
+++ app/src/main/java/com/adc/team/adc_team_mobil/PantallaPrincipal.java
@@ -0,0 +1,103 @@
+package com.adc.team.adc_team_mobil;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * Clase de la pantalla principal del login
+ */
+
+public class PantallaPrincipal extends AppCompatActivity {
+    //Declaración de variables
+    private TextView tvResultado,tvUsuari,tvPwd,tvId,tvRol;
+    Button btnLogOut;
+    private static final String TAG = "Resposta server :";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_principal);
+
+        tvUsuari    = (TextView) findViewById(R.id.tvUsuari);
+        tvPwd       = (TextView) findViewById(R.id.tvPwd);
+        tvId        = (TextView) findViewById(R.id.tvId);
+        tvRol       = (TextView) findViewById(R.id.tvRol);
+
+        String usuari  = getIntent().getStringExtra("usuari");
+        String pwd     = getIntent().getStringExtra("pwd");
+        String  id     = getIntent().getStringExtra("id");
+        String  rol    = getIntent().getStringExtra("rol");
+
+        tvUsuari.setText(usuari);
+        tvPwd.setText(pwd);
+        tvId.setText(id);
+        tvRol.setText(rol);
+
+        btnLogOut = (Button) findViewById(R.id.btn_logout);
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Task2().execute(tvUsuari.getText().toString());
+            }
+        });
+    }
+
+    class Task2 extends AsyncTask<String,Void, String> {
+
+        @Override
+        protected void onPreExecute(){
+            btnLogOut.setEnabled(false);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+
+                Socket sc;
+                sc = new Socket("192.168.0.15", 5000);
+                DataInputStream in = new DataInputStream(sc.getInputStream());
+                DataOutputStream out = new DataOutputStream(sc.getOutputStream());
+
+                // Llegir la resposta del servidor al establir la connexió
+                String resposta_svr = in.readUTF();
+                Log.i(TAG,resposta_svr);
+                Log.i(TAG, String.valueOf(String.valueOf(tvUsuari.getText().toString())) );
+                Log.i(TAG, String.valueOf(String.valueOf( tvPwd.getText().toString())) );
+
+                //Enviem resposta al servidor amb el usuari i la contrasenya i 0
+                out.writeUTF("LOGIN,"+ String.valueOf(tvUsuari.getText().toString()) + ","
+                        + String.valueOf(tvPwd.getText().toString()) + ","
+                        + String.valueOf(tvId.getText().toString()));
+
+                //Executem la consulta de la crida per sortir
+                out.writeUTF("USER_EXIT");
+
+            } catch (IOException  e) {
+                e.printStackTrace();
+            }
+
+            return strings[0];
+        }
+
+        @Override
+        protected void onPostExecute(String s){
+
+            Intent intent = new Intent(PantallaPrincipal.this,MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
